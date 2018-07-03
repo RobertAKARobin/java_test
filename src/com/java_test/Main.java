cc8bc9434982e2a458ad8aa06c1163f728e4d25f
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-03 15:50:54 -0500

    Initial commit

---
+++ src/com/java_test/Main.java
@@ -0,0 +1,16 @@
+package com.java_test;
+
+import static java.lang.System.out;
+
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length > 0){
+			out.println("Hello, " + args[0] + "!");			
+		}
+	}
+
+}
